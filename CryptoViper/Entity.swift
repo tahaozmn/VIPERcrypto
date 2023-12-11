b2fab82475f90f029cdb48610b48c5c902ae50ef
Taha ÖZMEN <139132956+tahaozmn@users.noreply.github.com>
2023-12-11 10:49:23 +0300

    Add files via upload

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Taha Özmen on 6.12.2023.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
